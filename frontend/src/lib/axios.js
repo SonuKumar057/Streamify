580592f44333ec4649102b57936a6c57ce3ae8c7
SonuKumar057 <sonukumar6204648@gmail.com>
2025-09-28 15:14:25 +0530

    Updated

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://streamify-ruby.vercel.app/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://streamify-9dd5.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
